e903017f251ff80949304ffbcbdbfc54e5c94c8f
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-09-03 13:42:36 +0200

    basic loop

--- Math.cs
+++ Math.cs
@@ -6,6 +6,14 @@ namespace Cs_dice_1
     {
         // sum of rolls
         // roll random number
+        static public Cs_dice_1.Data<byte>.Packet Roll(byte max_num)
+        {
+            Cs_dice_1.Data<byte>.Packet res;
+            res.Status = 0;
+            res.Payload = 9;
+
+            return res;
+        }
         // coinflip enum
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Cs_dice_1
+﻿using System.Runtime.Serialization;
+
+namespace Cs_dice_1
 {
      public class Program
     {
@@ -9,7 +11,29 @@
             // accept ndD from cmd
             // if follow, roll dice 
             // if not follow, show command 
-            System.Console.WriteLine("byte is from {0} to {1}", Byte.MinValue, Byte.MaxValue);
+
+            System.Console.WriteLine(@"[PH] welcome text"); //TODO: change to welcome message
+
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    System.Console.WriteLine("please enter a valid dice roll");
+                }
+                else if (input == @"/bye")
+                {
+                    System.Console.WriteLine("goodbye");
+                    break;
+                }
+                else
+                {
+                    var roll = Cs_dice_1.Math.Roll(20);
+                    //    if(roll.Status )
+                    System.Console.WriteLine(roll.Payload);
+                }
+
+            }
 
 
 
